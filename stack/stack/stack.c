7a6a8af29c1a10cdb3950d05eed0fafe5bf8a27c
DS <1838738883@qq.com>
2021-01-24 19:15:28 +0800

    栈练习（完成）

---
+++ stack/stack/stack.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+typedef int SLDatatype;
+typedef struct stack
+{
+	SLDatatype* _data;
+	int _size;
+	int _capacity;
+}stack;
+
+//栈初始化
+void stackInit(stack* st)
+{
+	st->_data = NULL;
+	st->_capacity = st->_size = 0;
+}
+
+//栈的增容
+void checkCapacity(stack* st)
+{
+	if (st->_capacity == st->_size) {
+		int newCapacity = st->_capacity == 0 ? 1 : 2 * st->_capacity;
+		st->_data = (int*)realloc(st->_data, sizeof(int) * newCapacity);
+		st->_capacity = newCapacity;
+	}
+}
+
+//入栈：尾插
+void stackPush(stack* st, SLDatatype val)
+{
+	if (st == NULL)
+		return;
+	checkCapacity(st);
+	st->_data[st->_size++] = val;
+}
+
+//出栈：尾删
+void stackPop(stack* st)
+{
+	if (st == 0)
+		return;
+	if (st->_size > 0)
+		st->_size--;
+}
+
+//显示栈顶的数据
+SLDatatype stackTop(stack* st)
+{
+	return st->_data[st->_size - 1];
+}
+void test()
+{
+	stack st;
+	stackInit(&st);
+	stackPush(&st, 1);
+	stackPush(&st, 2);
+	stackPush(&st, 3);
+	stackPush(&st, 4);
+}
+int main()
+{
+	test();
+	return 0;
+}

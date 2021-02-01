652bf8150de3f51c5e17551671ca393e6f90cbe8
DS <1838738883@qq.com>
2021-02-01 18:08:17 +0800

    栈 队列_oj （完成）

--- queue satck_oj/queue satck_oj/queue stack_oj.c
+++ queue satck_oj/queue satck_oj/queue stack_oj.c
@@ -488,3 +488,92 @@ void myQueueFree(MyQueue* obj) {
     free(obj);
 
 }
+
+
+
+//循环队列
+
+
+typedef struct {
+    int* _data;
+    //第一个元素的位置，队头元素；
+    int _front;
+    //最后一个元素的下一个位置
+    int _rear;
+    int _k;
+
+} MyCircularQueue;
+
+
+MyCircularQueue* myCircularQueueCreate(int k) {
+    MyCircularQueue* mq = (MyCircularQueue*)malloc(sizeof(MyCircularQueue));
+    mq->_data = (int*)malloc(sizeof(int) * (k + 1));
+    mq->_front = mq->_rear = 0;
+    mq->_k = k;
+    return mq;
+
+}
+
+bool myCircularQueueIsEmpty(MyCircularQueue* obj) {
+    if (obj->_front == obj->_rear)
+        return true;
+    return false;
+
+}
+
+bool myCircularQueueIsFull(MyCircularQueue* obj) {
+    if ((obj->_rear + 1) % (obj->_k + 1) == obj->_front)
+        return true;
+    else
+        return false;
+}
+bool myCircularQueueEnQueue(MyCircularQueue* obj, int value) {
+    //判断队列是否满
+    if (myCircularQueueIsFull(obj))
+        return false;
+    obj->_data[obj->_rear++] = value;
+    //判断队尾是否越界
+    if (obj->_rear > obj->_k)
+    {
+        obj->_rear = 0;
+    }
+    return true;
+
+}
+
+bool myCircularQueueDeQueue(MyCircularQueue* obj) {
+    //判断队列是否为空
+    if (myCircularQueueIsEmpty(obj))
+        return false;
+    //出队
+    obj->_front++;
+    //判断队头是否越界
+    if (obj->_front > obj->_k)
+        obj->_front = 0;
+    return true;
+
+}
+
+int myCircularQueueFront(MyCircularQueue* obj) {
+    if (myCircularQueueIsEmpty(obj))
+        return -1;
+    return obj->_data[obj->_front];
+
+}
+
+int myCircularQueueRear(MyCircularQueue* obj) {
+    if (myCircularQueueIsEmpty(obj))
+        return -1;
+    if (obj->_rear != 0)
+        return obj->_data[obj->_rear - 1];
+    return obj->_data[obj->_k];
+
+}
+
+
+
+void myCircularQueueFree(MyCircularQueue* obj) {
+    free(obj->_data);
+    free(obj);
+
+}

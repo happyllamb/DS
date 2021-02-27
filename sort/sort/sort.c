ceeec02168bc001645049b484437a59023814384
happyllamb <1838738883@qq.com>
2021-02-27 17:40:47 +0800

    排序

--- sort/sort/sort.c
+++ sort/sort/sort.c
@@ -9,12 +9,35 @@ void insertSort(int* arr, int n)
 	{
 		int end = i - 1;//end : 有序元素的最后一个位置
 		int data = arr[i];//保存此次需要排序的元素
-		while (end >= 0 && arr[end] >= data)
+		while (end >= 0 && arr[end] > data)
 		{
 			arr[end + 1] = arr[end];//大的数据向后移动
 			end--;
 		}
 		arr[end + 1] = data;
+	}
+}
+
+//希尔排序
+void shellSort(int* arr, int n)
+{
+	//gap：同组数据，相邻位置的间隔
+	int gap = n;
+	while (gap > 1)
+	{
+		//最后一趟排序，间隔必须是1，保证所有数据在同一组
+		gap = gap / 3 + 1;
+		for (int i = gap; i < n; i++)
+		{
+			int end = i - gap;//同组有序数据的最后位置
+			int data = arr[i];
+			while (end >= 0 && data < arr[end])
+			{
+				arr[end + gap] = arr[end];
+				end -= gap;
+			}
+			arr[end + gap] = data;
+		}
 	}
 }
 
@@ -32,7 +55,7 @@ void test()
 {
 	int arr[] = { 10,5,8,3,6,9,1,4,2,7 };
 	printSort(arr, sizeof(arr) / sizeof(arr[0]));
-	insertSort(arr, sizeof(arr) / sizeof(arr[0]));
+	shellSort(arr, sizeof(arr) / sizeof(arr[0]));
 	printSort(arr, sizeof(arr) / sizeof(arr[0]));
 }
 int main()
